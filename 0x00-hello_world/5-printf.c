7c5d6280b82ee1ac6809b0ffcead979e8aa64994
Joebim <josey359gmail.com>
2023-03-16 10:19:10 -0700

    cfile 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - head function
  * printf - function printing strring
- * return: 0 if code runs
+ * Return: Always 0 (Success
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
