2f372e1d90253e95f7ebaae44141a0743c51bde1
Joebim <josey359gmail.com>
2023-03-16 14:36:51 -0700

    cfile 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * char - message:array of strings
+ * int - length: size of message string
+ * return 1 when code success
+ */
+
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	int length = sizeof(string) - 1;
+
+	write(2, string, length);
+	return (1);
+}
