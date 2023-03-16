18f817c20415301f173ae985b9341fd1db5a8156
Joebim <josey359gmail.com>
2023-03-16 09:27:52 -0700

    cfile 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - head function
+ * printf - function printing strring
+ * return: 0 if code runs
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
