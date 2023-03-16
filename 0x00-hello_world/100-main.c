b81c96d1e60783d120d250143f68f86570ff2959
Joebim <josey359gmail.com>
2023-03-16 12:57:09 -0700

    cfile 100

---
+++ 0x00-hello_world/100-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - main function
+ * printf - print string
+ * return: Always 0 (Success
+ */
+
+int main(void)
+{
+	prinf("ALX is so coooooooool!");
+	return (0);
+}
