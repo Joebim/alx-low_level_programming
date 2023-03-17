76b5588924f37a1f6288ba33394cba6528fe1caf
Joebim <josey359gmail.com>
2023-03-17 16:40:28 -0700

    variable file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 89; num1++) 
+	for (num1 = 0; num1 < 10; num1++) 
 	{
-		for (num2 = 0; num2 <= 89; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			if (num1 != num2)
 			{
