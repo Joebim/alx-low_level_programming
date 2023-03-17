6ebe44ecd8285a0cdab38757d459298193419a77
Joebim <josey359gmail.com>
2023-03-17 16:35:35 -0700

    variable file 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (succesd)
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 89; num1++) 
+	{
+		for (num2 = 0; num2 <= 89; num2++)
+		{
+			if (num1 != num2)
+			{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
