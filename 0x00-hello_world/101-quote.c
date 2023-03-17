782f7d7c727345013b72e3c721b5138e42935aa4
Joebim <josey359gmail.com>
2023-03-16 17:23:01 -0700

    cfile 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,12 @@
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - main function entry point
- * char - message:array of strings
- * int - length: size of message string
  * Return: return 1 when code success
  */
 
 int main(void)
 {
-	char string1[] = "and that piece of art is useful\"";
-
-	char string2[] = " - Dora Korpar, 2015-10-19\n";
-
-	strcat(string1, string2);
-
-	int length = sizeof(string1) - 1;
-
-	write(2, string1, length);
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 54);
 	return (1);
 }
