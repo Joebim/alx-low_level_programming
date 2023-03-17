738052c9eda29d6384dc39d8afeeec0c8a5aaf33
Joebim <josey359gmail.com>
2023-03-17 10:20:38 -0700

    variable file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Alway 0 (success)
+ */
+
+int main(void)
+{
+	char letters = "abcdefghijklmnopqrstuvwxyz\n"; /*Declare variable letters*/
+
+	putchar(letters); /*Output letters to standard output*/
+	return (0);
+}
