08eee9828973076b9fb64e40fdcca26f0da0c093
Joebim <josey359gmail.com>
2023-03-17 15:38:43 -0700

    variable file 8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0')
+		if (number)
+		{
+			putchar(', ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
