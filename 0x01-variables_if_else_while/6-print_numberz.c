03a52a08a887f6b45dcf7ca7010464811e0fea15
Joebim <josey359gmail.com>
2023-03-17 14:33:25 -0700

    variable file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (success)
+ */
+
+in main(void)
+{
+	char num;
+
+	for (num = o; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
