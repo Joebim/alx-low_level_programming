4a1a68633a3f58fa11d458b4b23f88780f51c7eb
Joebim <josey359gmail.com>
2023-03-17 10:23:31 -0700

    variable file 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char letters = "abcdefghijklmnopqrstuvwxyz\n"; /*Declare variable letters*/
+	char letters* = "abcdefghijklmnopqrstuvwxyz\n"; /*Declare variable letters*/
 
 	putchar(letters); /*Output letters to standard output*/
 	return (0);
