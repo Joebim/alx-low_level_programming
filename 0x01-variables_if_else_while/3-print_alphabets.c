7c19e69e11560530a4fc353f28271d18e741e9b1
Joebim <josey359gmail.com>
2023-03-17 12:23:37 -0700

    variable file 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	char letters;
 
-	for (letters = a; letters < z; letters++) /*Loop through letters a to z*/
+	for (letters = a; letters <= z; letters++) /*Loop through letters a to z*/
 	{
 		putchar(letters); /*print letters to standard output*/
 	}
 
-	for (letters = A; letters < Z; letters++) /*Loop through letters A to Z*/
+	for (letters = A; letters <= Z; letters++) /*Loop through letters A to Z*/
 	{
 		putchar(letters); /*print letters to standard output*/
 	}
