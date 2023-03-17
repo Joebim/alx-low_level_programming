34a3c7cdda22831b770fdc33742cb8a4478346b5
Joebim <josey359gmail.com>
2023-03-17 11:50:47 -0700

    variable file 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	char *letters = "abcdefghijklmnopqrstuvwxyz\n"; /*Declare variable letters*/
-	
+
 	int i = 0; /* Initialize i to zero */
 
-	for(; letters[i] != '\0'; i++) /*Loop between letters*/
+	for (; letters[i] != '\0'; i++) /*Loop between letters*/
 	{
 		putchar(letters[i]); /*Print string to standard output*/
 	}
