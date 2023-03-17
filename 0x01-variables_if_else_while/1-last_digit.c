443cb2e87a5687120c69a7f9334012be87a6cf88
Joebim <josey359gmail.com>
2023-03-17 09:00:30 -0700

    variable file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,31 +7,32 @@
  * initialize varible n
  * last_n - function to get last character
  * if else condition
- * Return: return 0 if codd succesd
+ * Return: return 0 if code success
  */
+
+int last_n(int number)
+        {                                                                 int reminder;
+
+        reminder = number % 10;                                                   return (reminder);                                        }
+
 int main(void)
 {
 	int n;
 
-	int last_n(number)
-	{
-	int reminder;
-
-	reminder = number % 10;
-		return (reminder);
-	}
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
+	int ln = last_n(n)
+
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, last_n(n));
+		printf("Last digit of %d is %d and is greater than 5", n, ln);
 	} else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, last_n(n));
+		printf("Last digit of %d is %d and is 0", n, ln);
 	} else if ((n < 6) && !0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_n(n));
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, ln);
 	}
 	return (0);
 }
