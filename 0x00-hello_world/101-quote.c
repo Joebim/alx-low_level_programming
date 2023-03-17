3a3fb43662a3f1f50fddb9fb2cecfb27abb7ee7c
Joebim <josey359gmail.com>
2023-03-16 17:39:43 -0700

    cfile 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,9 @@
 
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 54);
+	write(STDERR_FILENO,
+		"and that piece of art is useful\" - Dora Korpar, "
+		"2015-10-19\n", 48);
+
 	return (1);
 }
