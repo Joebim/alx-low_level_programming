102bb4fd9cfb1081ac0ea127a4d5a4110af29878
Joebim <josey359gmail.com>
2023-03-17 18:30:03 -0700

    variable file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++) 
+	for (num1 = 0; num1 < 9; num1++) 
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
-			if (num1 != num2 && num2 != (0 || 1) && num1 != 9)
+			if (num2 != num1)
 			{
 			putchar(num1 + '0');
 			putchar(num2 + '0');
-			}
-			if (num1 != 8 && num2 != 9) {
-			putchar(',');
-			putchar(' ');
+			if (num1 == 8 && num2 == 9) {
+                        putchar(',');
+                        putchar(' ');
+                        }
 			}
 		}
 	}
