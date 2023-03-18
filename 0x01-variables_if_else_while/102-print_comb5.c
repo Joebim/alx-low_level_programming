5c521bf6a18fa99e08fbe2364c1ce440820ab89f
Joebim <josey359gmail.com>
2023-03-17 19:42:14 -0700

    variable file 100

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-        int num1, num2;
+	int num1, num2;
 
-        for (num1 = 0; num1 <= 98; num1++)
-        {
-                for (num2 = num1 + 1; num2 <= 99; num2++)
-                {
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
 			putchar(' ');
@@ -22,8 +22,8 @@ int main(void)
 			continue;
 			putchar(',');
 			putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
