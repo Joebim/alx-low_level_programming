b85cfe6ad57cb737f60223758da130503a25e4c6
Joebim <josey359gmail.com>
2023-03-17 18:35:01 -0700

    variable file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,12 +17,11 @@ int main(void)
 			{
 			putchar(num1 + '0');
 			putchar(num2 + '0');
+			}
 			if (num1 == 8 && num2 == 9) {
-			continue;
                         putchar(',');
                         putchar(' ');
                         }
-			}
 		}
 	}
 	putchar('\n');
