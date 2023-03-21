3c366925a8ea4c700cefed2536574f3f82eb694b
Joebim <josey359gmail.com>
2023-03-21 16:30:30 -0700

    [ADD] file 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - Print sign of numbers
+ * @n: Character to be inputed
+ * Return: 1 if n is higher than 0, 0 if 0 and -1 if n is lesser than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	} else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	} else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
