e315f487043ed2a9f3c2d0e74b8626ff371ea290
Joebim <josey359gmail.com>
2023-03-21 11:50:34 -0700

    [ADD] changes to function file 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+/**
+ * print_alphabet - function from header
+ * Return: null
+ */
+
 void print_alphabet(void)
 {
 	char n = 'a';
 
-	while(n <= 'z')
+	while (n <= 'z')
 	{
 		_putchar(n);
 		n++;
