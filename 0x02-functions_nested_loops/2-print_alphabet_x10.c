c5e266d311ed10de6c9637c7c28acd21b87b5da6
Joebim <josey359gmail.com>
2023-03-21 12:10:40 -0700

    [ADD] file 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function called from header
+ * Return: null
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int times;
+
+	times = 0;
+	letter = 'a';
+
+	while (times > 10)
+	{
+		while (letter >= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		times++;
+	}
+	_putchar('\n')
+}
