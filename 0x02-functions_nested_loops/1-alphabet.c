b952d3aa5cac1008567332114ca62ed4aefdfcd2
Joebim <josey359gmail.com>
2023-03-21 08:37:41 -0700

    functions: file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main-1.h"
+
+/**
+ * main - Entry point of the project
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main-1.h
@@ -0,0 +1,17 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+	int n = a;
+
+	while(n <= z)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n')
+}
+
+#endif /* MYHEADER_H */
