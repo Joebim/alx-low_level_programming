c3d8f9b97e74254f3e212cf512d85568db3dbe92
Joebim <josey359gmail.com>
2023-03-21 15:35:03 -0700

    [ADD] file 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabets
+ * @c - character to be checked
+ * Return: 1 if is sn alphabet, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= (a || A) && c <= (z || Z))
+	{
+		return (1);
+	} else
+		return (0);
+}
