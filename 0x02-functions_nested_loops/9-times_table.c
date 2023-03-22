4ef4533b673885d56ac0ac051fcf00b8f1bd6065
Joebim <josey359gmail.com>
2023-03-21 23:23:28 -0700

    [ADD] file 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void times_table(void)
+{
+	int columns, rows, ops;
+
+	for (columns = 0; columns <= 9; columns++)
+	{
+		for (rows = 0; rows <= 9; rows++)
+		{
+			ops = columns * rows;
+
+			if (ops <= 9)
+			{
+				if (rows > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(ops + '0');
+			} else {
+				if (rows > 0)
+				{
+					_putchar('0');
+					_putchar(' ');
+				}
+				_putchar(ops / 10 + '0');
+				_putchar(ops % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+
+
+}
