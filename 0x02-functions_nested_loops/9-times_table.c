5f7df6269a6f1a7ea855012e7d14019744cae9c3
Joebim <josey359gmail.com>
2023-03-21 23:32:13 -0700

    [ADD] file 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,8 +22,11 @@ void times_table(void)
 					_putchar(' ');
 					_putchar(' ');
 				}
+				_putchar(' ');
 				_putchar(ops + '0');
-			} else {
+			}
+			else
+			{
 				if (rows > 0)
 				{
 					_putchar('0');
