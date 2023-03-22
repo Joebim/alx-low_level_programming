586656638ca1501be9cb9ae0df48f665bf349e90
Joebim <josey359gmail.com>
2023-03-22 05:56:52 -0700

    [FIX] file 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,18 +4,28 @@
  *
  */
 
+void print_number(int n) {
+    if (n >= 10) {
+        print_number(n / 10);
+    }
+    putchar(n % 10 + '0');
+    putchar(',');
+    putchar(' ');
+}
+
 void print_to_98(int n) {
     int i;
 
     if (n <= 98) {
         for (i = n; i < 98; i++) {
-            putchar(i);
+            print_number(i);
         }
     } else {
         for (i = n; i > 98; i--) {
-            putchar(i);
+            print_number(i);
         }
     }
 
-    putchar(98);
+    print_number(98);
+    putchar('\n');
 }
