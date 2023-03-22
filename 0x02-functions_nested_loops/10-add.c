1cd55d93f5d68183a4c0e8fe803a163e0577479f
Joebim <josey359gmail.com>
2023-03-22 03:19:40 -0700

    [ADD] file 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,23 +1,26 @@
 #include "main.h"
 
 /**
- *
+ * add - Add two values
+ * @a: First parameter
+ * @b: Second parameter
+ * Return: Add parameters a & b
  */
 
 int add(int a, int b)
 {
 	/* Check if both numbers are negative */
-    if (a < 0 && b < 0) {
-        return a + b;
-    }
+	if (a < 0 && b < 0)
+		return (a + b);
 
-    /* Check if only one number is negative */
-    if (a < 0) {
-        return b + a;
-    } else if (b < 0) {
-        return a - b;
-    }
+	/* Check if only one number is negative */
+	if (a < 0)
+		return (b + a);
+	else if (b < 0)
+	{
+		return (a - b);
+	}
 
-    /* If both numbers are positive, just add them together */
-    return a + b;
+	/* If both numbers are positive, just add them together */
+	return (a + b);
 }
