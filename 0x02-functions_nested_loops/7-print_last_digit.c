93a0d24ddb3f817c80f900ee0c145955751e0de1
Joebim <josey359gmail.com>
2023-03-21 22:41:22 -0700

    [FIX] file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,29 +9,29 @@
 
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    /* handle special case for INT_MIN */
-    if (n == INT_MIN)
-    {
-        last_digit = 8;
-    }
-    else
-    {
-        if (n < 0)
-        {
-            n = -n;
-        }
+	/* handle special case for INT_MIN */
+	if (n == INT_MIN)
+	{
+		last_digit = 8;
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n = -n;
+		}
 
-        last_digit = n % 10; /* get the absolute value and modulo 10 */
+		last_digit = n % 10; /* get the absolute value and modulo 10 */
 
-        /* handle letters and convert to ASCII code */
-        if (n < 0 || n > 9)
-        {
-            n %= 10;
-        }
-    }
+		/* handle letters and convert to ASCII code */
+		if (n < 0 || n > 9)
+		{
+			n %= 10;
+		}
+	}
 
-    _putchar(last_digit + '0'); /* convert to ASCII code */
-    return last_digit;
+	_putchar(last_digit + '0'); /* convert to ASCII code */
+	return last_digit;
 }
