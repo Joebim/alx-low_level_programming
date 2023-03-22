1ba9500552d268bda673d81017267ca70a55d9d2
Joebim <josey359gmail.com>
2023-03-22 03:03:26 -0700

    [ADD] file 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,14 +8,14 @@ int add(int a, int b)
 {
 	/* Check if both numbers are negative */
     if (a < 0 && b < 0) {
-        return _(abs(a) + _abs(b));
+        return a + b;
     }
 
     /* Check if only one number is negative */
     if (a < 0) {
-        return b - _abs(a);
+        return b - a;
     } else if (b < 0) {
-        return a - _abs(b);
+        return a - b;
     }
 
     /* If both numbers are positive, just add them together */
