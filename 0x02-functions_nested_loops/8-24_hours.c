ef7fbf9e7da0250b918eeec9bfa76989616da1f8
Joebim <josey359gmail.com>
2023-03-21 22:59:24 -0700

    [ADD] file 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			_putchar(hours / 10 + '0');
-                        _putchar(hours % 10 + '0');
+			_putchar(hour / 10 + '0');
+                        _putchar(hour % 10 + '0');
                         _putchar(':');
-                        _putchar(minutes / 10 + '0');
-                        _putchar(minutes % 10 + '0');
+                        _putchar(minute / 10 + '0');
+                        _putchar(minute % 10 + '0');
                         _putchar('\n');
 		}
 	}
