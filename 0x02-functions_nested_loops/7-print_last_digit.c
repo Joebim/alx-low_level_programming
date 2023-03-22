2ec73755ba1df65737db677857fbd94f9b1c544e
Joebim <josey359gmail.com>
2023-03-21 21:19:17 -0700

    [ADD] file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * Return: n as last dight
+ */
+
+int print_last_digit(int)
+{
+	int n %= 10 /*Set n to modulos of 10 to get last digit*/
+	return (n);
+}
