dbb271d77d8c8d9887575f4c1b8484d15d434a54
Joebim <josey359gmail.com>
2023-03-22 04:18:55 -0700

    [ADD] changes to 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,35 +6,11 @@
 
 void print_to_98(int n)
 {
-	if (n < 0)
-	{
-		for (; n <= 98; n++)
-		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	if (n > 0 && n > 98)
-	{
-		for (; n <= 98; n++)
-		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	if (n > 98)
-	{
-		for (; n >= 98; n--)
-		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	if (n == 98)
+	for (; n != 98; n < 98 ? n++ : n--)
 	{
 		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
 	}
+	_putchar(98);
 }
