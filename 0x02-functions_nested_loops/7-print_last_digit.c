163f2f676dfdfc11fc2a8efbe9ab324d1d61ce85
Joebim <josey359gmail.com>
2023-03-21 21:48:42 -0700

    [FIX] file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,9 +5,18 @@
  * Return: n as last dight
  */
 
-int print_last_digit(int num)
+int print_last_digit(int n)
 {
-	int last_digit = _abs(num) % 10; /* get the absolute value and modulo 10 */
-	int n = last_digit + '0'; /* convert to ASCII code */
-	return n;
+	int last_digit;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+	int last_digit = n % 10; /* get the absolute value and modulo 10 */
+
+	int num = last_digit + '0'; /* convert to ASCII code */
+
+	return num;
 }
