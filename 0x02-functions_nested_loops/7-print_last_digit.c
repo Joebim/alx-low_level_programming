8e1770064bb53ff3996f853d7aed2dafaf7b4c0c
Joebim <josey359gmail.com>
2023-03-21 22:14:31 -0700

    [FIX] file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,10 +16,16 @@ int print_last_digit(int n)
 	}
 
 	/* handle letters and convert to ASCII code */
-        if (n < 0 || n > 9)
-        {
-                n %= 10;
-        }
+	if (n < 0 || n > 9)
+	{
+		n %= 10;
+	}
+
+	/* handle special case for INT_MIN */
+	if (n == 0 && last_digit != 0)
+	{
+		n = INT_MAX % 10 + 1;
+	}
 
 	last_digit = n % 10; /* get the absolute value and modulo 10 */
 
