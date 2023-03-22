327eb07b1b484278d69d7231a54e9f26ffd43387
Joebim <josey359gmail.com>
2023-03-22 04:27:14 -0700

    [ADD] changes to 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,11 @@
 
 void print_to_98(int n) {
     if (n <= 98) {
-        for (int i = n; i < 98; i++) {
+        for (; i < 98; i++) {
             std::cout << i << ", ";
         }
     } else {
-        for (int i = n; i > 98; i--) {
+        for (; i > 98; i--) {
             std::cout << i << ", ";
         }
     }
