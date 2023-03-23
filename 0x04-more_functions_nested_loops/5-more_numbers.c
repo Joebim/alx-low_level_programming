93a00793eca4f08de4c9a4c3e0a3bfd8b21e2706
Joebim <josey359gmail.com>
2023-03-23 11:20:46 -0700

    [ADD] changes to 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,15 @@ void more_numbers(void)
 {
 	int n, times;
 
-	for (times = 0; times <= 10; times++) /*loop of ten numbers*/
+	for (times = 0; times < 10; times++) /*loop of ten numbers*/
 	{
 		for (n = 0; n <= 14; n++) /*loop number from one to fourteen*/
 		{
-			putchar(n);
+			if (times >= 10)
+			{
+				putchar(times / 10 + '0');
+			}
+			putchar(times % 10 + '0');
 		}
 	}
 	putchar('\n');
