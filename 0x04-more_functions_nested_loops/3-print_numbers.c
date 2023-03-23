dd45568ca18d1e142fdc03aef22ec5f03b9814fa
Joebim <josey359gmail.com>
2023-03-23 09:01:27 -0700

    [ADD] file 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int num; /*Initialize number variable*/
+
+	num = 0;
+
+	while (num <= 9) /*loop to print numbers*/
+	{
+		_putchar(num + '0');
+		num++;
+	}
+}
