9f9873afa171e12fe07caca1f801a54205ec2807
Joebim <josey359gmail.com>
2023-03-23 09:43:56 -0700

    [ADD] file 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - print a loop of ten numbers from 1 to 14
+ */
+
+void more_numbers(void)
+{
+	int n, times;
+
+	for (times = 0; times <= 10; times++) /*loop of ten numbers*/
+	{
+		n = 0;
+		for (n = 0; n <= 14; n++) /*loop number from one to fourteen*/
+		{
+			putchar(n + '0');
+		}
+	}
+	putchar('\n');
+}
