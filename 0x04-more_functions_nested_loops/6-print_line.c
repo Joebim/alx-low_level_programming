f12cd2dc94e8e16a0a10b5609cde80b1ef8b06c9
Joebim <josey359gmail.com>
2023-03-23 13:35:09 -0700

    [ADD] file 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,14 @@ void print_line(int n)
 {
 	if (n > 0) {
 		int i;
+
 		for (i = 0; i < n; i++) {
 			_putchar('_');
 		}
 	_putchar('\n');
 	}
-	else
-		_putchar('\n');
+	else if (n <= 0)
+	{
+		_putchar('\n')
+	}
 }
