7105a58674bf51b7c6328aa2484ace27d46dd60f
Joebim <josey359gmail.com>
2023-03-23 09:17:21 -0700

    [ADD] file 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	char num; /*Initialize number variable*/
 
-	num = 0;
+	num = '0';
 
-	while (num < 9) /*loop to print numbers*/
+	while (num < '9') /*loop to print numbers*/
 	{
 		putchar(num);
 		num++;
