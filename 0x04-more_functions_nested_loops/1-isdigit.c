0f2b4da69363b16b8ab0a633bfff1da31177400d
Joebim <josey359gmail.com>
2023-03-23 03:36:00 -0700

    [ADD] file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c <= 9 && !(c > 0))
+	{
+		return (1);
+	} else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+int mul(int a, int b)
+{
+	int add;
+
+	add = a * b
+	return (add);	
+}
