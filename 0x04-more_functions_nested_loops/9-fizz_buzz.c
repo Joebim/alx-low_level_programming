73c019986700aa7def2b17e10e73729e8062230e
Joebim <josey359gmail.com>
2023-03-24 06:52:51 -0700

    [ADD] file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,31 +11,31 @@
 
 int main(void)
 {
-    int i;
-
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("fizzbuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("buzz");
-        }
-        else
-        {
-            printf("%d", i);
-        }
-	if (i != 100)
+	int i;
+	
+	for (i = 1; i <= 100; i++)
 	{
-		printf(" ");
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzbuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
-    }
-   printf("\n");
-    return (0);
+	printf("\n");
+	return (0);
 }
