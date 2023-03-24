518aff8b571d6ca9cda4f9bf5123cf9915980f8d
Joebim <josey359gmail.com>
2023-03-24 06:00:39 -0700

    [ADD] file 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,7 +4,16 @@
  *
  */
 
-void print_square(int size)
-{
-	
+void print_square(int size) {
+    if (size <= 0) {
+        _putchar('\n');
+        return;
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
 }
