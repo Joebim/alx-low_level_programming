2b46dfc7bbd9d1404a2d325fc365691d2c0a5a51
Joebim <josey359gmail.com>
2023-03-24 06:41:46 -0700

    [ADD] file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,17 +1,21 @@
-#include "main.h"
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- *
+ * main - Entry point
+ * Description: prints the numbers from 1 to 100, followed by a new line.
+ * But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i;
+    int i;
 
-    for (i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
             _putchar('F');
             _putchar('i');
             _putchar('z');
@@ -21,19 +25,25 @@ int main(void)
             _putchar('z');
             _putchar('z');
             _putchar(' ');
-        } else if (i % 3 == 0) {
+        }
+        else if (i % 3 == 0)
+        {
             _putchar('F');
             _putchar('i');
             _putchar('z');
             _putchar('z');
             _putchar(' ');
-        } else if (i % 5 == 0) {
+        }
+        else if (i % 5 == 0)
+        {
             _putchar('B');
             _putchar('u');
             _putchar('z');
             _putchar('z');
             _putchar(' ');
-        } else {
+        }
+        else
+        {
             int num = i;
             do {
                 _putchar(num % 10 + '0');
@@ -43,5 +53,5 @@ int main(void)
         }
     }
     _putchar('\n');
-    return 0;
+    return (0);
 }
