0e20966aae71ef401af4988871687b389fa34779
Joebim <josey359gmail.com>
2023-03-24 04:51:00 -0700

    [ADD] file 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,20 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *
  */
 
-void print_diagonal(int n) {
-	if (n <= 0) {
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
 		_putchar('\n');
 		return;
 	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			if (j == i) {
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j == i)
+			{
 				_putchar('\\');
-			} else {
+			}
+			else
+			{
 				_putchar(' ');
 			}
 		}
