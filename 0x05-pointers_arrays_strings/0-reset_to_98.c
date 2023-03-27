bb2686b359f2565c63212ac1d67e9806cd3a75bd
Joebim <josey359gmail.com>
2023-03-27 06:36:53 -0700

    [ADD] file README.md, 0-reset_to_98.c & [ADD] description in readme file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset pointer to an int to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Array an pointer ALX tasks for low level programming
