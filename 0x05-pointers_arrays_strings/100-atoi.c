53f729f3b92aa9ed964ecc775796c0423ccc9fc7
Joebim <josey359gmail.com>
2023-03-27 16:29:23 -0700

    [ADD] file 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,30 +10,29 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	int result = 0;
+    if (s == NULL)
+        return 0;
 
-	int i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+            {
+                return sign == -1 ? INT_MIN : INT_MAX;
+            }
+            result = result * 10 + (s[i] - '0');
+        }
+        if (result != 0 && !(s[i + 1] >= '0' && s[i + 1] <= '9'))
+            break;
+        i++;
+    }
 
-	if (s == NULL)
-		return (0);
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
-			result = result * 10 + (s[i] - '0');
-
-		if (result * sign > INT_MAX)
-                                return INT_MAX;
-                        else if (result * sign < INT_MIN)
-                                return INT_MIN;
-
-		if (result != 0 && !(s[i + 1] >= '0' && s[i + 1] <= '9'))
-			break;
-		i++;
-}
-	return (sign * result);
+    return sign * result;
 }
