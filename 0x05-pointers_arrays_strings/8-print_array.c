85f3cdeb9172720d9722ce05bf090c679220d280
Joebim <josey359gmail.com>
2023-03-27 14:40:33 -0700

    [ADD] file and C code to file 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an arry to a given number
+ * @a: Array to be printed
+ * @n: number of times to print array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
