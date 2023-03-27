da3324436765531148d3b8f4d92eb691dcec63ce
Joebim <josey359gmail.com>
2023-03-27 06:48:33 -0700

    [ADD] fiie & C code 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ * @a: first value to be swapped
+ * @b: second value to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
