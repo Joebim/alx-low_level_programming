c4230f296432930c72ef8a1743aaccc486d4c266
Joebim <josey359gmail.com>
2023-03-27 16:15:15 -0700

    [ADD] file 100-atoi.cgti

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,9 +4,29 @@
 /**
  * _atoi - convert string to integer
  * @s: String parameter
+ * Return: Return sign * result
  */
 
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int sign = 1;
+
+	int result = 0;
+
+	int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			result = result * 10 + (s[i] - '0');
+		if (result != 0 && !(s[i + 1] >= '0' && s[i + 1] <= '9'))
+			break;
+		i++;
+	}
+	return (sign * result);
 }
