262faace6a7bf54773ab26f87d9d4634072d43ae
Joebim <josey359gmail.com>
2023-03-27 15:07:37 -0700

    [FIX] file 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,9 +5,11 @@
  * *_strcpy - copy pointed string to buffer
  * @dest: given destination
  * @src: given String
+ * Return: return 0
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
+	return (0);
 }
