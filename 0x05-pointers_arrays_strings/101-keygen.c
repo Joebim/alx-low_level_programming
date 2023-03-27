dd2c5466c82217d07f4f2abb07adeee1810990f0
Joebim <josey359gmail.com>
2023-03-27 16:59:55 -0700

    [ADD] file 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - generates a random password that matches the hash in 101-crackme
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-    char password[16];
-    int i;
+    char password[15];
+    int i, sum, diff, rand_num;
 
     srand(time(NULL));
 
-    for (i = 0; i < 15; i++)
+    /* Generate random password */
+    for (i = 0; i < 12; i++)
     {
-        password[i] = rand() % ('~' - '!' + 1) + '!';
+        rand_num = rand() % 62;
+        if (rand_num < 26)
+            password[i] = 'a' + rand_num;
+        else if (rand_num < 52)
+            password[i] = 'A' + (rand_num - 26);
+        else
+            password[i] = '0' + (rand_num - 52);
     }
-    password[i] = '\0';
 
-    printf("%s\n", password);
+    /* Calculate expected hash */
+    sum = password[0] + password[1] + password[2] + password[3] + password[4];
+    diff = password[5] - password[6];
+    printf("Hash: %d\n", sum ^ 0x3b);
+    printf("Expected hash: %d\n", diff);
+
+    /* Print password */
+    password[12] = '\n';
+    password[13] = '\0';
+    printf("%s", password);
+
     return (0);
 }
