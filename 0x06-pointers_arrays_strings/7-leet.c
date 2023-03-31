4102d70d8e759315f45132a9a5110d9c48370195
Joebim <josey359gmail.com>
2023-03-30 19:18:47 -0700

    [ADD] Code solution to file 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - encode string in a number
+ * @str: given string
+ * Return: String
+ */
+
+char *leet(char *str)
+{
+	const char *leet_chars = "AEOTL";
+	
+	const char *leet_subs = "43071";
+	
+	int i, j;
+	
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; leet_chars[j]; j++)
+		{
+			if (str[i] == leet_chars[j] || str[i] == leet_chars[j] + 32)
+			{
+				str[i] = leet_subs[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
