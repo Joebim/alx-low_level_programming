85a8f1fded3939e5786cd8cba425c4b25c9df73c
Joebim <josey359gmail.com>
2023-03-30 18:55:59 -0700

    [ADD] code solution to file 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * *cap_string - capitalize string
+ * @str: String
+ * Return: String
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	while (str[i]) {
+		if (i == 0 || isspace(str[i - 1]))
+		{
+			str[i] = toupper(str[i]);
+		}
+		i++;
+	}
+	return (str);
+}
