13603ded77deeb58b9b45d0c6453419b1ba7a0b6
Joebim <josey359gmail.com>
2023-03-30 18:02:17 -0700

    [FIX] betty issues in file 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,10 +2,11 @@
 #include <string.h>
 
 /**
- * _strncpy - copy the string pointed to by src
+ * *_strncpy - copy the string pointed to by src
  * @dest: destination
  * @src: Source text string
  * @n: number of bytes to use
+ * Return: Destination
  */
 
 char *_strncpy(char *dest, char *src, int n)
