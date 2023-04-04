b98f9d5aaff929c5404ee70c233c3271369499cb
Joebim <josey359gmail.com>
2023-04-03 17:04:02 -0700

    [ADD] code solution to file 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strchr - Returns a pointer to the first occurrence of a character
+ * @s: string
+ * @c: character
+ * Return: result of function
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
