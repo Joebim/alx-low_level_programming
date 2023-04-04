aa9122241eab960c11b7e984e1d561ba40da0416
Joebim <josey359gmail.com>
2023-04-03 17:45:27 -0700

    [ADD] code solution to file 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.b"
+#include <string.h>
+
+/**
+ * *_strstr - function finds the first occurrence of the substring
+ * @haystack: string
+ * @needle: string
+ * Return: output of function
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (sttstr(haystack, needle));
+}
