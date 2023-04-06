4220d8e049f77148df32166138e5a9bea0df5146
Joebim <josey359gmail.com>
2023-04-06 05:53:02 -0700

    [ADD] code solution to file 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,53 +1,28 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - computes the natural square root of a number
- * @n: the number to compute the square root of
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * @root: square root of n.
  *
- * Return: the natural square root of n, or -1 if n does not have a
- * natural square root
+ * Return: square root of n, or -1 if n does not have a natural square root.
  */
 
-int helper(int n, int low, int high);
+int square_root(int n, int root)
+{
+	if (root * root == n)
+		return (root);
+
+	if (root * root > n)
+		return (-1);
+
+	return (square_root(n, root + 1));
+}
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0) /* error case */
-	{
-		return -1;
-	}
-	else if (n == 0 || n == 1) /* base case */
-	{
-		return n;
-	}
-	else /* recursive case */
-	{
-		return helper(n, 0, n);
-	}
-}
+	if (n < 0)
+		return (-1);
 
-int helper(int n, int low, int high)
-{
-	int mid, sqr;
-
-	if (low > high) /* error case */
-	{
-		return -1;
-	}
-
-	mid = (low + high) / 2;
-	sqr = mid * mid;
-
-	if (sqr == n) /* base case */
-	{
-		return mid;
-	}
-	else if (sqr < n) /* look for square root in upper half */
-	{
-		return helper(n, mid + 1, high);
-	}
-	else /* look for square root in lower half */
-	{
-		return helper(n, low, mid - 1);
-	}
+	return (square_root(n, 0));
 }
