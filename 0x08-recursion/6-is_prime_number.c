2862d58e9574e27c58c7eb74976182e20695de94
Joebim <josey359gmail.com>
2023-04-06 06:11:54 -0700

    [ADD] code solution to file  6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,13 +12,13 @@ int is_prime_helper(int n, int i);
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
-    if (n == 2 || n == 3)
-        return (1);
-    if (n % 2 == 0 || n % 3 == 0)
-        return (0);
-    return (is_prime_helper(n, 5));
+	if (n <= 1)
+		return (0);
+	if (n == 2 || n == 3)
+		return (1);
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	return (is_prime_helper(n, 5));
 }
 
 /**
@@ -31,9 +31,9 @@ int is_prime_number(int n)
  */
 int is_prime_helper(int n, int i)
 {
-    if (i * i > n)
-        return (1);
-    if (n % i == 0 || n % (i + 2) == 0)
-        return (0);
-    return (is_prime_helper(n, i + 6));
+	if (i * i > n)
+		return (1);
+	if (n % i == 0 || n % (i + 2) == 0)
+		return (0);
+	return (is_prime_helper(n, i + 6));
 }
