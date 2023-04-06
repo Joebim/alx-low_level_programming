3f0e7e7078a01ecf9fa1fadd735c11a03982c481
Joebim <josey359gmail.com>
2023-04-06 05:00:57 -0700

    [ADD] Code solution to file 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion -  returns the value of x raised to the power of y
+ * @x: regular number
+ * @y: power
+ * Return: the value of x raised to the power of y, or -1 if y is negative
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return 1;
+	}
+
+	if (y > 0)
+	{
+		return x * _pow_recursion(x, y - 1);
+	}
+
+	return -1;
+}
