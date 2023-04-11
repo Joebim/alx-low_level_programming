cf4ac2859ac083f0cd76d8b90a96f9e76b1745ff
Joebim <josey359gmail.com>
2023-04-10 20:01:08 -0700

    [ADD] code solution to file 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * is_digit - checks if a character is a digit
+ * @c: character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * print_number - prints an integer using putchar
+ * @n: number to print
+ */
+
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * main - Adds positive numbers passed as arguments.
+ * @argc: number of array pointer argv
+ * @argv: array pointer
+ * Return: 0 when success
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, c, v;
+
+	if (argc < 2)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+
+	for (c = 1; c < argc; c++)
+	{
+		for (v = 0; argv[c][v]; v++)
+		{
+			if (!is_num(argv[i][j]))
+			{
+				_putchar('E');
+				_putchar('r');
+				_putchar('r');
+				_putchar('o');
+				_putchar('r');
+				_putchar('\n');
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	_putchar(sum + '0');
+	_putchar('\n');
+	return (0);
+}
