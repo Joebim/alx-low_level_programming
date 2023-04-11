0c6790d9f53e3fa83da72c8169580e9ef2716f8b
Joebim <josey359gmail.com>
2023-04-10 18:38:20 -0700

    [ADD] code solution to file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print programme name
+ * @argc: number of array pointer argv
+ * @argv: array pointer
+ * Return: 0 when success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	_putchar(argc + '0');
+	return (0);
+}
