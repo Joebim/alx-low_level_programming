d3da38dba4320b2f3aab5db4ce954b027a346f0f
Joebim <josey359gmail.com>
2023-04-12 08:06:42 -0700

    [ADD] code solution to file 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - return pointer to memory that includes string
+ * @str: Given string
+ * Return: NULL if str is null and strdup function
+ */
+
+char *_strdup(char *str)
+{
+	int length;
+
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	length = strlen(str);
+
+	p = malloc(sizeof(*str) * length + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	strcpy(p, str);
+	return (strdup(p));
+}
