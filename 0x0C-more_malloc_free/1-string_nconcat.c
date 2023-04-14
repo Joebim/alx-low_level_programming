4bb4df4c3434636ef8aec512796bfe3c2001ceba
Joebim <josey359gmail.com>
2023-04-13 18:16:25 -0700

    [ADD] Code solution to file 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,43 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * string_nconcat - allocates memory using malloc.
  * @s1: first string
  * @s2: second string
- * Return: 
+ * Return:
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
+        char *p;
 
-	size_t l1, l2;
+        unsigned int l1, l2;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	l1 = strlen(s1);
-	l2 = strlen(s2);
-	if (n >= l2)
-	{
-		n = l2;
-	}
-	p = (char*) malloc(s1 + n + 1);
+        if (s1 == NULL)
+        {
+                s1 = "";
+        }
+        if (s2 == NULL)
+        {
+                s2 = "";
+        }
 
-	if (p == NULL)
-	{
-		return NULL;
-	}
-	memcpy(p, s1, l1);
-	memcpy(p + l1, s2, n);
-	p(l1 + n) = '\0';
-	return (p);
+        l1 = strlen(s1);
+        l2 = strlen(s2);
+        if (n >= l2)
+        {
+                n = l2;
+        }
+        p = malloc(sizeof(char) * (1 + n + 1));
+
+        if (p == NULL)
+        {
+                return (NULL);
+        }
+        memcpy(p, s1, l1);
+        memcpy(p + l1, s2, n);
+        p[l1 + n] = '\0';
+        return (p);
 }
