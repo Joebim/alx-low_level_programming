04f5dd90c4d7c7985dab8a387f8bafc44a92c8ef
Joebim <josey359gmail.com>
2023-04-14 18:35:22 -0700

    [ADD] Code solution to file 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+**
+ * _calloc -  allocate memory for an array, using malloc.
+ * @nmemb: Passed number
+ * @size: size of value on memory
+ * Return: Null and pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+	{
+		return(NULL);
+	}
+
+	memset(p, 0, nmemb * size);
+
+	return(p);
+}
