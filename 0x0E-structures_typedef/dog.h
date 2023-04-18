b412a56dd02b0fc4e30378057346cc4a33fa158f
Joebim <josey359gmail.com>
2023-04-17 17:44:49 -0700

    [ADD] Files and code solution for header file  dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure name dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
 struct dog
 {
 	char *name;
@@ -8,4 +15,4 @@ struct dog
 	char *owner;
 };
 
-#endif 
+#endif
