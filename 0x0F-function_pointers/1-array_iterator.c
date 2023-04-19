c07eee6b39b2958d38fccb320ee232131e661ffa
Joebim <josey359gmail.com>
2023-04-19 16:38:17 -0700

    [ADD] code solution to file 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * array_iterator - execute func given as a param on each element of an array
+ * @array: Array
+ * @size: Array size
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int num;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	for (num = 0; num < size; num++)
+	{
+		action(array[num]);
+	}
+}
