590540ae1815028db684c420f1e032b620b2f37c
Joebim <josey359gmail.com>
2023-04-19 14:56:11 -0700

    [ADD] code solution to file 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name.
+ * @name: string name
+ * @f: Function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f) (name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+ALX low level programming task on Function Pointers
