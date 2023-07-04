4518dd6b52c3b5deecce74eaf1e4d7ceff849d0b
Joebim <josey359@gmail.com>
2023-07-03 18:49:31 -0700

    [ADD] code solution to file 03, 04, 05, 06 and header file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * premain - function that is executed before main
+ * Return: void
+ */
+void __attribute__((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
