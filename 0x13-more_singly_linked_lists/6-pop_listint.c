ba671c7aaed563e32d2c8eaae017a56cb0bd3b4f
Joebim <josey359@gmail.com>
2023-07-05 20:02:17 -0700

    [ADD] code solution to task 6 - 9

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+nclude "lists.h"
+/**
+ *  * pop_listint - delete the head listint_t list
+ *   * @head: head of the listint_t list
+ *    * Return: returns the listint_t data n
+ *     */
+int pop_listint(listint_t **head)
+{
+		int n;
+			listint_t *temp;
+
+				if (head == NULL || *head == NULL)
+							return (0);
+					n = (*head)->n;
+						temp = *head;
+							*head = (*head)->next;
+								free(temp);
+									return (n);
+}
