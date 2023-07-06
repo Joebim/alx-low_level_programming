eb70ba5e9fe4cfc90683e8f6606c0ad5dda49536
Joebim <josey359@gmail.com>
2023-07-05 20:00:17 -0700

    [ADD] code solution to task 1-5

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+nclude "lists.h"
+/**
+ *  * add_nodeint - add new node to the beginning of a list_t list
+ *   * @head: head of the listint_t list
+ *    * @n: n property of list_t
+ *     * Return: new node if SUCCESSFUL or NULL if not
+ *      */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+		listint_t *node;
+
+			node = malloc(sizeof(listint_t));
+				if (node == NULL)
+							return (NULL);
+					node->n = n;
+						node->next = *head;
+							*head = node;
+								return (node);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+nclude "lists.h"
+/**
+ *  * free_listint - free a listint_t list
+ *   *  @head: head of the listint_t list
+ *    */
+void free_listint(listint_t *head)
+{
+		listint_t *temp;
+
+			while (head != NULL)
+					{
+								temp = head;
+										head = head->next;
+												free(temp);
+													}
+}
